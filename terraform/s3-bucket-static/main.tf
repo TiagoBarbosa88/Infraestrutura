d5bc2f4e1410bab380813b7d4e0d62f1a2956284
TiagoBarbosa88 <sombr43@gmail.com>
2024-02-06 14:33:05 -0300

    update

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "static_site_bucket" {
-  bucket = "static-site-${var.bucket_name}"
+     bucket = "static-site-${var.bucket_name}"
 
-  tags = {
-    Name       = "Static Site Bucket"
-    Enviroment = "Dev"
-  }
+    tags = {
+        Name = "Static Site Bucket"
+        Environment = "Production"
+    }
 }
 
 resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
@@ -20,17 +20,16 @@ resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
-  bucket = aws_s3_bucket.static_site_bucket.id
+    bucket = aws_s3_bucket.static_site_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
-
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -38,8 +37,8 @@ resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
 
 resource "aws_s3_bucket_acl" "static_site_bucket" {
   depends_on = [
-    aws_s3_bucket_public_access_block.static_site_bucket,
-    aws_s3_bucket_ownership_controls.static_site_bucket,
+	aws_s3_bucket_public_access_block.static_site_bucket,
+	aws_s3_bucket_ownership_controls.static_site_bucket,
   ]
 
   bucket = aws_s3_bucket.static_site_bucket.id
