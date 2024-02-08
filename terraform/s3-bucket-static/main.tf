ad5e5d7caa6ffffe1f1885fc424df8c9e7c8390f
TiagoBarbosa88 <sombr43@gmail.com>
2024-02-08 16:18:55 -0300

    feat: update polices IAM

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -44,3 +44,20 @@ resource "aws_s3_bucket_acl" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
   acl    = "public-read"
 }
+
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = data.aws_s3_bucket.selected-bucket.id
+  policy = data.aws_iam_policy_document.iam-policy-1.json
+}
+data "aws_iam_policy_document" "iam-policy-1" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+    actions = ["S3:GetObject"]
+ principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
